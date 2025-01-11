cff9ac367602be63a6b9dd1f2e6953628040dbb3
5nnea <147231961+5nnea@users.noreply.github.com>
2025-01-11 19:53:32 +0900

    트리거 표시 정보 업데이트

--- Last War/Assets/Scripts/GameManager.cs
+++ Last War/Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public TrigData[] trigDatas;
     public int waves = 0;
+
     [SerializeField] GameObject trigs;
+    [SerializeField] Material goodTrig;
+    [SerializeField] Material badTrig;
 
     void Start(){
         InvokeRepeating("CreatTrig",1f,10f);
@@ -22,7 +26,25 @@ public class GameManager : MonoBehaviour
         waves++;
         if(trigDatas.Length < waves){
             SceneManager.LoadScene(1);
-        }
-        Instantiate(trigs, trigs.transform.position, trigs.transform.rotation);
+        }   
+        SetTrigInfo();     
+    }
+
+    void SetTrigInfo(){
+        GameObject newTrigs = Instantiate(trigs, trigs.transform.position, trigs.transform.rotation);
+        GameObject trig1 = newTrigs.transform.GetChild(0).gameObject;
+        GameObject trig2 = newTrigs.transform.GetChild(1).gameObject;
+        TMP_Text trigText1 = trig1.transform.GetChild(0).GetComponent<TMP_Text>();
+        TMP_Text trigText2 = trig2.transform.GetChild(0).GetComponent<TMP_Text>();
+        
+        trigText1.text = trigDatas[waves-1].sign1 == "*" ? "X" + trigDatas[waves-1].num1.ToString() : 
+        trigDatas[waves-1].sign1.ToString() + trigDatas[waves-1].num1.ToString();
+
+        trigText2.text = trigDatas[waves-1].sign2 == "*" ? "X" + trigDatas[waves-1].num2.ToString() :
+        trigDatas[waves-1].sign2.ToString() + trigDatas[waves-1].num2.ToString();
+
+        trig1.GetComponent<MeshRenderer>().material = trigDatas[waves-1].sign1 == "-" ? badTrig : goodTrig;
+        trig2.GetComponent<MeshRenderer>().material = trigDatas[waves-1].sign2 == "-" ? badTrig : goodTrig;
+
     }
 }
